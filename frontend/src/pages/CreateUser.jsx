74b949ab6470e08a1c6624b19881cdc03d0db7fe
kasuni2001-maker <136631162+kasuni2001-maker@users.noreply.github.com>
2023-09-14 22:10:16 +0530

    Add files via upload

---
+++ frontend/src/pages/CreateUser.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+
+const CreateUser = () => {
+  const [Name, setName] = useState('');
+  const [author, setNIC] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
+
+  const handleSaveUser= () => {
+    const data = {
+      Name,
+      NIC,
+    };
+    setLoading(true);
+    axios
+      .post('http://localhost:5555/books', data)
+      .then(() => {
+        setLoading(false);
+        enqueueSnackbar('User Created successfully', { variant: 'success' });
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        // alert('An error happened. Please Chack console');
+        enqueueSnackbar('Error', { variant: 'error' });
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Create User</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Name</label>
+          <input
+            type='text'
+            value={Name}
+            onChange={(e) => setName(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>NIC</label>
+          <input
+            type='text'
+            value={NIC}
+            onChange={(e) => setNIC(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2  w-full '
+          />
+        </div>
+         
+        <button className='p-2 bg-sky-300 m-8' onClick={handleSaveUser}>
+          Save
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CreateUser
