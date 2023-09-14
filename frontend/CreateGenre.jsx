972c291e91056fef59bbc9afb3e33987c14fe63e
kasuni2001-maker <136631162+kasuni2001-maker@users.noreply.github.com>
2023-09-15 01:53:24 +0530

    Add files via upload

---
+++ frontend/CreateGenre.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+
+const CreateGenre = () => {
+  const [Genre, setGenre] = useState('');
+  const [BookId, setBookId] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
+
+  const handleSaveGenre = () => {
+    const data = {
+      Genre,
+      BookId,
+    };
+    setLoading(true);
+    axios
+      .post('http://localhost:5173/Genre', data)
+      .then(() => {
+        setLoading(false);
+        enqueueSnackbar('Genre Created successfully', { variant: 'success' });
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        // alert('An error happened. Please Chack console');
+        enqueueSnackbar('Error', { variant: 'error' });
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Create Genre</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Genre</label>
+          <input
+            type='text'
+            value={Genre}
+            onChange={(e) => setGenre(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>BookId</label>
+          <input
+            type='text'
+            value={author}
+            onChange={(e) => setBookId(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2  w-full '
+          />
+        </div>
+        <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
+          Save
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CreateGenre
